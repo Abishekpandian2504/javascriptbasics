fcf6af073e998e91cd2d69cb314fbd6df572a468
Abishekpandian2504 <abishekpandian2504@gmail.com.com>
2023-12-07 15:07:53 +0530

    first

---
+++ index.js
@@ -0,0 +1,63 @@
+    //    alert (" hello ");
+
+    // //    alert( 4 + 5);
+
+    //    console.log("new 123 ");
+
+//2nd    
+// let name = "Abishek";
+// let msg = "hello i'm "+ name + " app developer";
+
+// alert (msg);
+
+// console.log(msg);
+
+//3rd mistake here(undefined)
+// let name = 23 ;
+// let msg ;
+
+// alert (msg);
+
+// console.log(name);
+
+//4th
+// let name = "Abishek ";
+// let msg = "hey i am " + name + " learning javascript"; 
+
+// console.log(msg);
+
+//5th let and const diff ( let la name more time declare panikalam so we use const )
+// // let name = "Abishek ";
+// // name = "hello";
+// const name = "Abishek ";
+// const dob = "25=04-1999";
+// //name = "hello";
+// let msg = "hey i am " + name + " learning javascript and born on " +  dob ; 
+// //msg = " hey dude";
+
+// console.log(msg);
+
+
+//6th data types modal
+// let name = "Aravinth";  //string
+
+// let age = 23;      //int or num
+
+
+// let isYounger = true;  //boolean
+
+
+// let lastName;    // undefined
+// let lastName = undefined;
+
+
+// let trophy = null;    //null we add extra things inn output result
+// trophy = trophy1;
+
+
+//7th
+
+
+
+
+
