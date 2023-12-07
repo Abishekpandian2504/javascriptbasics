bd86b38b1c52e1eee2b1fc04b8213b7a71454760
Abishekpandian2504 <abishekpandian2504@gmail.com.com>
2023-12-08 00:24:10 +0530

    second

--- index.js
+++ index.js
@@ -55,9 +55,143 @@
 // trophy = trophy1;
 
 
-//7th
+//7th brower console la u use ( typeof ) u see it is string or other
+// let name = "Aravinth";  //string
+
+// let age = 23;      //int or num
 
 
+// let isYounger = true;  //boolean
 
+
+// let lastName;    // undefined
+// //let lastName = undefined;
+
+
+// let trophy = null;    //null we add extra things inn output result
+// //trophy = trophy1;
+
+//8th using object
+// // let name = "Aravinth";  //string
+// // let age = 23;      //int or num
+// // let isYounger = true;  //boolean
+// // let address = " chennai, tamilnadu";
+
+// let persondetail = {
+//     name : "Aravinth",  //string
+//     age : 23,     //int or num
+//     isYounger : true,  //boolean
+//     address : " chennai, tamilnadu",
+
+// };
+
+// console.log(persondetail)
+
+//9th use notation to findout
+// let persondetail = {
+//     name : "Aravinth",  //string
+//     age : 23,     //int or num
+//     isYounger : true,  //boolean
+//     address : " chennai, tamilnadu",
+
+// };
+// // persondetail.age = 25;
+
+// // dot notation
+// console.log(persondetail.age);
+
+// // bracket notation
+// console.log(persondetail['address']);
+
+//10th object inside create one new obj
+// let persondetail = {
+//     name : "Aravinth",  //string
+//     age : 23,     //int or num
+//     isYounger : true,  //boolean
+//     address : " chennai, tamilnadu",
+//     parentname: {
+//         father : "Arjunan",
+//         mother : "Murugeswari"
+//     }
+
+// };
+// // persondetail.age = 25;
+
+// // dot notation
+// console.log(persondetail.age);
+// console.log(persondetail.parentname.father);
+
+// // bracket notation
+// console.log(persondetail['address']);
+
+//11th Arrays
+// let details = ["name" , "number", "email"];
+
+// details[6] = "address";
+
+// console.log(details);
+// console.log(details[5]);
+// console.log(details.length);
+
+//12th Functions and create variables ( y we use func because we create one func and call that func in multiple operations)
+// function newUser(firstName, lastName) {
+//     let message = "hello i'm " + firstName + " " + lastName + " & learning javascript";
+//     console.log(message)  
+// }
+// function operations(number1 , number2) {
+//  result = number1 + number2;
+//     console.log(result)
+//     console.log(number1 * number2);
+// }
+// // variable + operator = expression(algorithm)
+
+// newUser("Abishek", "pandian")
+// newUser("Abishek2", "pandian2")
+// operations(5,20)
+// operations(40,2)
+
+//13th assignment Operator
+// let x = 5;
+//  // x = x + 2,
+//     x *= 3
+// console.log(x)
+
+//14th arithmatic operator
+// let a = 3;
+
+// //console.log(a);
+// //increment
+//  console.log(++a);
+//  console.log(a++);
+//  console.log(a++);
+// //decrement
+//  console.log(--a);
+//  console.log(--a);
+//  console.log(a--);
+//  console.log(a--);
+
+//15th comparison operator 
+
+// console.log( 5 > 3)
+
+let x = 5;
+
+//relation operator
+console.log( x > 8);
+console.log( x <= 5);
+
+//equality operator
+console.log ( x == 5);
+console.log ( x != 5)
+
+//string comparison
+console.log( "abishek" < "aravinth")
+
+//comparison of diff type
+console.log ('5' < 4)  //smart JS consider 5 < 4
+console.log(true == 1 )  // true=1 , false=0
+
+
+ 
 
 
